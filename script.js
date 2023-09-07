ebfaa8968ec50be6c3044e2afaf99e4b73effd0b
sevaaaDev <136947471+sevaaaDev@users.noreply.github.com>
2023-09-07 16:34:41 +0700

    change to modular pattern

--- script.js
+++ script.js
@@ -1,110 +1,137 @@
-const table = document.getElementById("table");
-const dialog = document.querySelector("dialog");
-const btnShowDialog = document.getElementById("show-dialog");
-const btnCloseDialog = document.getElementById("close-dialog");
-const btnDelete = document.querySelectorAll("[data-index]");
-const input = {
-  title: document.getElementById("title"),
-  author: document.getElementById("author"),
-  pages: document.getElementById("pages"),
-  read: document.getElementById("finished"),
-};
-let library = [];
+const Library = () => {
+  let bookshelf = [];
 
-class Book {
-  constructor(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-  }
+  class Book {
+    constructor(title, author, pages, read) {
+      this.title = title;
+      this.author = author;
+      this.pages = pages;
+      this.read = read;
+    }
 
-  changeStatus() {
-    if (this.read === "Read") {
-      this.read = "Not read";
-    } else {
-      this.read = "Read";
+    changeStatus() {
+      if (this.read === "Read") {
+        this.read = "Not read";
+      } else {
+        this.read = "Read";
+      }
     }
   }
-}
 
-function addBookToTheList(title, author, pages, read) {
-  library.push(new Book(title, author, pages, read));
-}
-
-function resetDisplay() {
-  while (table.firstChild) {
-    table.removeChild(table.firstChild);
+  function getShelf() {
+    return bookshelf
   }
-}
 
-function displayBook() {
-  resetDisplay();
-  for (let i = 0; i < library.length; i++) {
-    const tr = document.createElement("tr");
-    table.append(tr);
-    const key = Object.keys(library[i]);
-    key.forEach((key) => {
+  function removeBook(index) {
+    bookshelf.splice(index, 1);
+  }
+
+  function changeStatus(index) {
+    bookshelf[index].changeStatus()
+  }
+  
+  function addBookToTheList(title, author, pages, read) {
+    bookshelf.push(new Book(title, author, pages, read));
+  }
+
+  return { addBookToTheList, removeBook, getShelf, changeStatus };
+};
+
+const displayController = () => {
+  const table = document.getElementById("table");
+  const dialog = document.querySelector("dialog");
+  const btnShowDialog = document.getElementById("show-dialog");
+  const btnCloseDialog = document.getElementById("close-dialog");
+  const btnDelete = document.querySelectorAll("[data-index]");
+  const input = {
+    title: document.getElementById("title"),
+    author: document.getElementById("author"),
+    pages: document.getElementById("pages"),
+    read: document.getElementById("finished"),
+  };
+
+  function resetDisplay() {
+    while (table.firstChild) {
+      table.removeChild(table.firstChild);
+    }
+  }
+
+  function displayBook(shelf) {
+    resetDisplay();
+    for (let i = 0; i < shelf.length; i++) {
+      const tr = document.createElement("tr");
+      table.append(tr);
+      const key = Object.keys(shelf[i]);
+      key.forEach((key) => {
+        const td = document.createElement("td");
+        td.innerText = shelf[i][key];
+        tr.append(td);
+      });
       const td = document.createElement("td");
-      td.innerText = library[i][key];
+      const td2 = document.createElement("td");
+      const btnRead = document.createElement("button");
+      btnRead.innerText = "Change Status";
+      btnRead.setAttribute("data-index", i);
+      td.append(btnRead);
+      const btnDelete = document.createElement("button");
+      btnDelete.setAttribute("data-index", i);
+      btnDelete.innerText = "Delete";
+      td2.append(btnDelete);
       tr.append(td);
-    });
-    const td = document.createElement("td");
-    const td2 = document.createElement("td");
-    const btnRead = document.createElement("button");
-    btnRead.innerText = "Change Status";
-    btnRead.setAttribute("data-index", i);
-    td.append(btnRead);
-    const btnDelete = document.createElement("button");
-    btnDelete.setAttribute("data-index", i);
-    btnDelete.innerText = "Delete";
-    td2.append(btnDelete);
-    tr.append(td);
-    tr.append(td2);
+      tr.append(td2);
+    }
   }
-}
 
-btnShowDialog.addEventListener("click", () => {
-  dialog.showModal();
-});
+  return {
+    table,
+    dialog,
+    btnShowDialog,
+    btnCloseDialog,
+    btnDelete,
+    input,
+    displayBook,
+  };
+};
 
-btnCloseDialog.addEventListener("click", () => {
-  if (input.read.checked) {
-    addBookToTheList(
-      input.title.value,
-      input.author.value,
-      input.pages.value,
-      "Read"
-    );
-    displayBook();
-  } else {
-    addBookToTheList(
-      input.title.value,
-      input.author.value,
-      input.pages.value,
-      "Not read"
-    );
-    displayBook();
-  }
-});
+const inputController = (() => {
+  const library = Library();
+  const display = displayController();
 
-window.addEventListener("click", (e) => {
-  if (e.target.dataset.index === undefined) {
-    return;
-  }
-  if (e.target.innerText === "Change Status") {
-    library[e.target.dataset.index].changeStatus();
-    displayBook();
-    return;
-  }
-  removeBook(e.target.dataset.index);
-  console.log(e.target.dataset.index);
-});
+  display.btnShowDialog.addEventListener("click", () => {
+    display.dialog.showModal();
+  });
 
-function removeBook(index) {
-  library.splice(index, 1);
-  displayBook();
-}
+  display.btnCloseDialog.addEventListener("click", () => {
+    if (display.input.read.checked) {
+      library.addBookToTheList(
+        display.input.title.value,
+        display.input.author.value,
+        display.input.pages.value,
+        "Read"
+      );
+      display.displayBook(library.getShelf());
+    } else {
+      library.addBookToTheList(
+        display.input.title.value,
+        display.input.author.value,
+        display.input.pages.value,
+        "Not read"
+      );
+      display.displayBook(library.getShelf());
+    }
+  });
 
-addBookToTheList("Sapiens", "Yuval Noah Harari", 500, "Read");
-displayBook();
+  window.addEventListener("click", (e) => {
+    if (e.target.dataset.index === undefined) {
+      return;
+    }
+    if (e.target.innerText === "Change Status") {
+      library.changeStatus(e.target.dataset.index);
+      display.displayBook(library.getShelf());
+      return;
+    }
+    library.removeBook(e.target.dataset.index);
+    display.displayBook(library.getShelf())
+    console.log(e.target.dataset.index);
+  });
+})()
