f5ffe48bf3ca7d8993de13c4a84c3e872b36b67c
sevaaaDev <136947471+sevaaaDev@users.noreply.github.com>
2023-09-07 15:50:22 +0700

    change func construtor to class

--- script.js
+++ script.js
@@ -1,98 +1,110 @@
-const table = document.getElementById('table')
-const dialog = document.querySelector('dialog')
-const btnShowDialog = document.getElementById('show-dialog')
-const btnCloseDialog = document.getElementById('close-dialog')
-const btnDelete = document.querySelectorAll('[data-index]')
+const table = document.getElementById("table");
+const dialog = document.querySelector("dialog");
+const btnShowDialog = document.getElementById("show-dialog");
+const btnCloseDialog = document.getElementById("close-dialog");
+const btnDelete = document.querySelectorAll("[data-index]");
 const input = {
-  title: document.getElementById('title'),
-  author: document.getElementById('author'),
-  pages: document.getElementById('pages'),
-  read: document.getElementById('finished')
+  title: document.getElementById("title"),
+  author: document.getElementById("author"),
+  pages: document.getElementById("pages"),
+  read: document.getElementById("finished"),
+};
+let library = [];
+
+class Book {
+  constructor(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
+
+  changeStatus() {
+    if (this.read === "Read") {
+      this.read = "Not read";
+    } else {
+      this.read = "Read";
+    }
+  }
 }
-let library = []
 
-function Book(title, author, pages, read) {
-  this.title = title
-  this.author = author
-  this.pages = pages
-  this.read = read
-}  
-
-function addBookToTheList(title, author, pages, read) {  
-  library.push(new Book(title, author, pages, read))
-}  
+function addBookToTheList(title, author, pages, read) {
+  library.push(new Book(title, author, pages, read));
+}
 
 function resetDisplay() {
   while (table.firstChild) {
-    table.removeChild(table.firstChild)
-  }  
+    table.removeChild(table.firstChild);
+  }
 }
 
 function displayBook() {
-  resetDisplay()
+  resetDisplay();
   for (let i = 0; i < library.length; i++) {
-    const tr = document.createElement('tr')
-    table.append(tr)
-    const key = Object.keys(library[i])
+    const tr = document.createElement("tr");
+    table.append(tr);
+    const key = Object.keys(library[i]);
     key.forEach((key) => {
-      const td = document.createElement('td')
-      td.innerText = library[i][key]
-      tr.append(td)
-    })  
-    const td = document.createElement('td')
-    const td2 = document.createElement('td')
-    const btnRead = document.createElement('button')
-    btnRead.innerText = 'Change Status'
-    btnRead.setAttribute('data-index', i)
-    td.append(btnRead)
-    const btnDelete = document.createElement('button')
-    btnDelete.setAttribute('data-index', i)
-    btnDelete.innerText = 'Delete'
-    td2.append(btnDelete)
-    tr.append(td)
-    tr.append(td2)
-  }  
-}  
+      const td = document.createElement("td");
+      td.innerText = library[i][key];
+      tr.append(td);
+    });
+    const td = document.createElement("td");
+    const td2 = document.createElement("td");
+    const btnRead = document.createElement("button");
+    btnRead.innerText = "Change Status";
+    btnRead.setAttribute("data-index", i);
+    td.append(btnRead);
+    const btnDelete = document.createElement("button");
+    btnDelete.setAttribute("data-index", i);
+    btnDelete.innerText = "Delete";
+    td2.append(btnDelete);
+    tr.append(td);
+    tr.append(td2);
+  }
+}
 
-btnShowDialog.addEventListener('click', () => {
-  dialog.showModal()
-})
+btnShowDialog.addEventListener("click", () => {
+  dialog.showModal();
+});
 
-btnCloseDialog.addEventListener('click', () => {
+btnCloseDialog.addEventListener("click", () => {
   if (input.read.checked) {
-    addBookToTheList(input.title.value, input.author.value, input.pages.value, 'Read')
-    displayBook()
+    addBookToTheList(
+      input.title.value,
+      input.author.value,
+      input.pages.value,
+      "Read"
+    );
+    displayBook();
   } else {
-    addBookToTheList(input.title.value, input.author.value, input.pages.value, 'Not read')
-    displayBook()
+    addBookToTheList(
+      input.title.value,
+      input.author.value,
+      input.pages.value,
+      "Not read"
+    );
+    displayBook();
   }
-})
+});
 
-window.addEventListener('click', (e) => {
+window.addEventListener("click", (e) => {
   if (e.target.dataset.index === undefined) {
-    return
+    return;
   }
-  if (e.target.innerText === 'Change Status') {
-    library[e.target.dataset.index].changeStatus()
-    displayBook()
-    return
+  if (e.target.innerText === "Change Status") {
+    library[e.target.dataset.index].changeStatus();
+    displayBook();
+    return;
   }
-  removeBook(e.target.dataset.index)
-  console.log(e.target.dataset.index)
-})
+  removeBook(e.target.dataset.index);
+  console.log(e.target.dataset.index);
+});
 
 function removeBook(index) {
-  library.splice(index, 1)
-  displayBook()
+  library.splice(index, 1);
+  displayBook();
 }
 
-Book.prototype.changeStatus = function() {
-  if (this.read === 'Read') {
-    this.read = 'Not read'
-  } else {
-    this.read = 'Read'
-  }
-}
-
-addBookToTheList('Sapiens', 'Yuval Noah Harari', 500, 'Read') 
-displayBook()
+addBookToTheList("Sapiens", "Yuval Noah Harari", 500, "Read");
+displayBook();
